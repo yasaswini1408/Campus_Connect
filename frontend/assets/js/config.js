2acd9fe23a9d2982d5a2922817ef871cd89a5c81
gitswati-27 <swatik2706@gmail.com>
2025-12-06 09:20:15 +0530

    added deployed backend url

--- frontend/assets/js/config.js
+++ frontend/assets/js/config.js
@@ -1,4 +1,4 @@
 export const BASE_URL =
   window.location.hostname === "localhost"
     ? "http://localhost:3000"
-    : "https://your-production-backend.onrender.com";
+    : "https://campus-connect-qpma.onrender.com";
